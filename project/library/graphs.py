d7d83a55fdb72ed574f7c0bc06de25b8e0c29d3a
Andrew Lazarev <andrew.lzrv@gmail.com>
2026-09-17 01:13:53 +0300

    Feat: done `get_graph_info` & `labeled_two_cycle_graph_to_dot` functions
    Signed-off-by: Andrew Lazarev <andrew.lzrv@gmail.com>

---
+++ project/library/graphs.py
@@ -0,0 +1,36 @@
+import cfpq_data
+from typing import Any, Iterable, NamedTuple
+from networkx import MultiDiGraph, nx_pydot
+
+
+class GraphInfo(NamedTuple):
+    vertices: int
+    edges: int
+    edgeLabels: set[str]
+
+
+def get_graph_info(name: str) -> GraphInfo:
+    path = cfpq_data.download(name)
+    graph = cfpq_data.graph_from_csv(path)
+
+    labels = set()
+
+    for _, _, label in graph.edges(data='label'):
+        labels.add(label)
+
+    return GraphInfo(
+        vertices=graph.number_of_nodes(),
+        edges=graph.number_of_edges(),
+        edgeLabels=labels,
+    )
+
+
+def labeled_two_cycle_graph_to_dot(n: int, m: int, common_node: int = 0, labels: tuple[str, str] = ('a', 'b'), filename: str | None = None) -> MultiDiGraph:
+    graph = cfpq_data.labeled_two_cycles_graph(
+        n, m, common_node=common_node, labels=labels)
+
+    if filename is not None:
+        graph_dot = nx_pydot.to_pydot(graph)
+        graph_dot.write(filename, format='raw')
+
+    return graph
